a939e098cfbb6038ab15b63f48a8a0a002a0f8a1
Jocelyn <deleojocelync@gmail.com>
2024-10-27 21:18:34 -0700

    Initial commit

---
+++ src/CSC450_CT1_mod1-1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+// #include <conio.h>  // Removed because it’s not available on macOS
+
+using namespace std;
+
+int main() {
+    cout << "Welcome to this C++ Program" << endl;
+    cout << "I have corrected all errors for this program." << endl;
+
+    // Code for fictional person's information
+        cout << "First Name: Harry" << endl;
+        cout << "Last Name: Styles" << endl;
+        cout << "Street Address: 111 Penwood Pl" << endl;
+        cout << "City: LA" << endl;
+        cout << "Zip Code: 90001" << endl;// getch();  // Optional: comment this out or remove entirely
+
+    return 0;
+}
---
+++ src/CSC450_CT1_mod1-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+// #include <conio.h>  // Removed because it’s not available on macOS
+
+using namespace std;
+
+int main() {
+    double myMoney = 1000.50;
+
+    cout << "Please be sure to correct all syntax errors in this program" << endl;
+    cout << "I have corrected all errors for this program." << endl;
+    cout << "The total amount of money available is = " << myMoney << endl;
+
+    // getch();  // Optional: comment this out or remove entirely
+
+    return 0;
+}
